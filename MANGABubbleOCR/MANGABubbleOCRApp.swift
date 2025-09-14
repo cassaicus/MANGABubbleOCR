69cf069011bf337556c1b2b19d3b654da445b0d4
cassaicus <cassaicus+x@gmail.com>
2025-09-14 10:15:06 +0900

    Initial Commit

---
+++ MANGABubbleOCR/MANGABubbleOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  MANGABubbleOCRApp.swift
+//  MANGABubbleOCR
+//
+//  Created by ibis on 2025/09/14.
+//
+
+import SwiftUI
+
+@main
+struct MANGABubbleOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
